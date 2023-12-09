6147c4d3a60a347660560863bc13092521acf24d
wangyiyuelva <yiyuwang891120@gmail.com>
2023-12-09 13:24:53 +0000

    add select_needed script

---
+++ select_needed.py
@@ -0,0 +1,31 @@
+import sys
+import random
+from Bio import SeqIO
+
+"""
+usage: python select_needed.py INPUT.fasta
+"""
+
+def read_input(file):
+    """
+    Function reads a fasta formatted file of protein sequences
+    """
+    # print("READING FASTA FILES")
+    records = []
+    for record in SeqIO.parse(file, "fasta"):
+        records.append(record)
+    return(records)
+
+
+if __name__ == "__main__":
+    target = {}
+    with open("experiment_ids.txt", "r") as f:
+        for i, line in enumerate(f): 
+            target[line[:-1]] = i
+    records = read_input(sys.argv[1])
+    for record in records:
+        if record.id in target:
+            output_file = "./input_data/input_" + "{:04d}".format(target[record.id]) + ".fa"
+            with open(output_file, "w") as fh_out:
+                fh_out.write(f">{record.description}\n")
+                fh_out.write(f"{record.seq}\n")
