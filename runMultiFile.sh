8c90a4da4b5781638425d3a9fcbf5c5fd3cb97f0
wangyiyuelva <yiyuwang891120@gmail.com>
2023-12-13 21:15:27 +0000

    hotfix: input path in script

--- runMultiFile.sh
+++ runMultiFile.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 cd /home/ec2-user/data/COMP0235
 source ~/data/virtualenv/venv/bin/activate
-for filename in test/*.fa; do
+for filename in input/*.fa; do
   python ./pipeline_script.py "$filename"
 done
